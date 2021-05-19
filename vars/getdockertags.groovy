2cb9bc67fbcbb7b66f91744dcc7a567cf8eaafab
Pavan <46976811+pavanreddymaley@users.noreply.github.com>
2021-05-18 19:49:03 -0500

    Update getdockertags.groovy

--- vars/getdockertags.groovy
+++ vars/getdockertags.groovy
@@ -1,15 +1,17 @@
 def call() {
-    def get = new URL("https://api.npoint.io/94c7dfacc8e334149afb").openConnection();
-    def getRC = get.getResponseCode();
+    script {
+        def get = new URL("https://api.npoint.io/94c7dfacc8e334149afb").openConnection();
+        def getRC = get.getResponseCode();
 
-    def slurper = new groovy.json.JsonSlurper()
-    def finalResult = "AB,CD,EF"
-    if(getRC.equals(200)) {
-        def response = get.getInputStream().getText();
-        def parsedResponse = slurper.parseText(response)
-        println(parsedResponse.results);
-        finalResult = parsedResponse.results
+        def slurper = new groovy.json.JsonSlurper()
+        def finalResult = "AB,CD,EF"
+        if(getRC.equals(200)) {
+            def response = get.getInputStream().getText();
+            def parsedResponse = slurper.parseText(response)
+            println(parsedResponse.results);
+            finalResult = parsedResponse.results
+        }
+
+        return finalResult
     }
-
-    return finalResult
 }
